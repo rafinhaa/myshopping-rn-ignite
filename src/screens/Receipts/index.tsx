c0c08da3d597842af26a4a2d02c958fd51ec8b4a
Rafael Soncine <rafael.soncine@meliuz.com.br>
2022-03-31 09:26:44 -0300

    Fundamentos do firebase - Storage - Dica para listar arquivos

--- src/screens/Receipts/index.tsx
+++ src/screens/Receipts/index.tsx
@@ -1,37 +1,45 @@
-import React from 'react';
-import { FlatList } from 'react-native';
+import React, { useEffect, useState } from "react";
+import { FlatList } from "react-native";
+import storage from "@react-native-firebase/storage";
 
-import { Container, PhotoInfo } from './styles';
-import { Header } from '../../components/Header';
-import { Photo } from '../../components/Photo';
-import { File } from '../../components/File';
-
-import { photosData } from '../../utils/photo.data';
+import { Container, PhotoInfo } from "./styles";
+import { Header } from "../../components/Header";
+import { Photo } from "../../components/Photo";
+import { File, FileProps } from "../../components/File";
 
 export function Receipts() {
+  const [photos, setPhotos] = useState<FileProps[]>([]);
+
+  useEffect(() => {
+    storage()
+      .ref("images")
+      .listAll()
+      .then((res) => {
+        const photos = res.items.map((item) => ({
+          name: item.name,
+          path: item.fullPath,
+        }));
+        setPhotos(photos);
+      });
+  }, []);
+
   return (
     <Container>
       <Header title="Comprovantes" />
 
       <Photo uri="" />
 
-      <PhotoInfo>
-        Informações da foto
-      </PhotoInfo>
+      <PhotoInfo>Informações da foto</PhotoInfo>
 
       <FlatList
-        data={photosData}
-        keyExtractor={item => item.name}
+        data={photos}
+        keyExtractor={(item) => item.name}
         renderItem={({ item }) => (
-          <File
-            data={item}
-            onShow={() => { }}
-            onDelete={() => { }}
-          />
+          <File data={item} onShow={() => {}} onDelete={() => {}} />
         )}
         contentContainerStyle={{ paddingBottom: 100 }}
         showsVerticalScrollIndicator={false}
-        style={{ width: '100%', padding: 24 }}
+        style={{ width: "100%", padding: 24 }}
       />
     </Container>
   );
