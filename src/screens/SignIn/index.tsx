5ce35365491a276f1f3d4e92575e708a9464c8a8
Rafael Soncine <rafael.soncine@meliuz.com.br>
2022-04-01 10:16:33 -0300

    Fundamentos do firebase - Autenticação - Criando uma conta de login com e-mail e senha

--- src/screens/SignIn/index.tsx
+++ src/screens/SignIn/index.tsx
@@ -1,6 +1,6 @@
-import React from "react";
+import React, { useState } from "react";
 
-import { firebase } from "@react-native-firebase/auth";
+import auth from "@react-native-firebase/auth";
 
 import { Container, Account, Title, Subtitle } from "./styles";
 import { ButtonText } from "../../components/ButtonText";
@@ -8,9 +8,23 @@ import { Button } from "../../components/Button";
 import { Input } from "../../components/Input";
 
 export function SignIn() {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
   const handleSignInAnonymously = async () => {
-    const { user } = await firebase.auth().signInAnonymously();
+    const { user } = await auth().signInAnonymously();
     console.log(user);
+  };
+
+  const handleCreateUserAccount = () => {
+    auth()
+      .createUserWithEmailAndPassword(email, password)
+      .then(() => {
+        alert("Usuário criado com sucesso!");
+      })
+      .catch((error) => {
+        alert(error.message);
+      });
   };
 
   return (
@@ -18,15 +32,22 @@ export function SignIn() {
       <Title>MyShopping</Title>
       <Subtitle>monte sua lista de compra te ajudar nas compras</Subtitle>
 
-      <Input placeholder="e-mail" keyboardType="email-address" />
+      <Input
+        placeholder="e-mail"
+        keyboardType="email-address"
+        onChangeText={setEmail}
+      />
 
-      <Input placeholder="senha" secureTextEntry />
+      <Input placeholder="senha" secureTextEntry onChangeText={setPassword} />
 
       <Button title="Entrar" onPress={handleSignInAnonymously} />
 
       <Account>
         <ButtonText title="Recuperar senha" onPress={() => {}} />
-        <ButtonText title="Criar minha conta" onPress={() => {}} />
+        <ButtonText
+          title="Criar minha conta"
+          onPress={handleCreateUserAccount}
+        />
       </Account>
     </Container>
   );
